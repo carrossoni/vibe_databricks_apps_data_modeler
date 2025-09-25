43ff7c839ecca79570875a51054892fff2ccd91f
Luiz Carrossoni <carrossoni@gmail.com>
2025-09-24 21:59:01 -0300

    Delete backend/models/__init__.py

--- backend/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .autoui import *
-from .data_modeling import (
-    DataModelProject, DataTable, TableField, DataModelRelationship,
-    DatabricksDataType, ForeignKeyReference, ExistingTableImport,
-    MetricView, MetricViewDimension, MetricViewMeasure, MetricViewJoin,
-    MetricSourceRelationship, TraditionalView
-)
-from .yaml_serializer import DataModelYAMLSerializer
